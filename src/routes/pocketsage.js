9bbcfa10ef59f72c62da7908a870bf4608a9ebc0
Nikita <nikmast45@yandex.ru>
2023-01-11 08:47:58 +0300

    1st version

---
+++ src/routes/pocketsage.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const router = express.Router()
+
+const user = {
+    "id": "1",
+    "name": "Вуймов Никита",
+    "isLogged": false
+}
+
+router.get('/pocketsages', (req, res) => {
+
+    res.render('./pocketsage/main.ejs', {
+        user: user
+    })
+})
+
+router.get('/pocketsages/ekj', (req, res) => {
+    res.render('./pocketsage/ekj.ejs', {
+        user:user
+    })
+})
+
+router.get('/pocketsages/actirovka', (req, res) => {
+    res.render('./pocketsage/actirovka.ejs', {
+        user:user
+    })
+})
+
+router.get('/pocketsages/notes', (req, res) => {
+    res.render('./pocketsage/notes.ejs', {
+        user:user
+    })
+})
+router.get('/pocketsages/links', (req, res) => {
+    res.render('./pocketsage/links.ejs', {
+        user:user
+    })
+})
+router.get('/pocketsages/calculator', (req, res) => {
+    res.render('./pocketsage/calculator.ejs', {
+        user:user
+    })
+})
+
+router.get('/pocketsages/log_in', (req, res) => {
+    res.render('./pocketsage/log_in.ejs', {
+        user:user
+    })
+})
+
+router.get('/pocketsages/card_settings', (req, res) => {
+    res.render('./pocketsage/card_settings.ejs', {
+        user:user
+    })
+})
+
+router.get('/pocketsages/error_feedback', (req, res) => {
+    res.render('./pocketsage/error_feedback.ejs', {
+        user:user
+    })
+})
+
+
+
+module.exports = router
---
+++ src/server.js
@@ -0,0 +1,21 @@
+// Require everything we need
+const express = require('express')
+require('dotenv').config({ path: './.env' })
+//app
+const app = express()
+
+app.use(express.urlencoded())
+app.set("view engine", "ejs")
+
+const logger = require('./middleware/logger')
+const indexRouter = require('./routes/pocketsage')
+const error_404 = require('./middleware/error-404')
+app.use('/', indexRouter)
+app.use(error_404)
+app.use(logger)
+
+
+//start
+const PORT = process.env.PORT || 3002
+app.listen(PORT)
+console.log(`Server is listening port ${PORT}`)
